a831fc24e341fafa63fbbbd9ec731f41f48d09cc
Tan Yanbo <tanyanbo95@hotmail.com>
2022-11-09 17:29:27 +0800

    add unit tests for print function

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest"
+import { beforeEach, describe, expect, it, MockInstance, vi } from "vitest"
 import { compileHtml, MyNode } from ".."
 
 describe("html compiler", () => {
@@ -122,5 +122,52 @@ describe("html compiler", () => {
     expect(() => {
       compileHtml(html)
     }).toThrow("Closing tag does not match opening tag")
+  })
+})
+
+describe("node print function", () => {
+  let spy: MockInstance
+
+  beforeEach(() => {
+    spy = vi.spyOn(console, "log")
+  })
+
+  it("should print when print is called", () => {
+    const node = new MyNode("div")
+    node.print()
+    expect(spy).toHaveBeenCalledOnce()
+  })
+
+  it("should print with correct indentation for a single element", () => {
+    const node = new MyNode("div")
+    node.print()
+    expect(spy).toBeCalledWith("div")
+  })
+
+  it("should print twice for a single element with one child element", () => {
+    const node = new MyNode("div", [new MyNode("p")])
+    node.print()
+    expect(spy).toHaveBeenCalledTimes(2)
+  })
+
+  it("should print with correct indentation for a single element with one child element", () => {
+    const node = new MyNode("div", [new MyNode("p")])
+    node.print()
+    expect(spy).toHaveBeenNthCalledWith(1, "div")
+    expect(spy).toHaveBeenNthCalledWith(2, "  p")
+  })
+
+  it("should print 3 times for a single element with one child element with text as its child", () => {
+    const node = new MyNode("div", [new MyNode("p", ["aaa"])])
+    node.print()
+    expect(spy).toBeCalledTimes(3)
+  })
+
+  it("should print with correct indentation for a single element with one child element with text as its child", () => {
+    const node = new MyNode("div", [new MyNode("p", ["aaa"])])
+    node.print()
+    expect(spy).toHaveBeenNthCalledWith(1, "div")
+    expect(spy).toHaveBeenNthCalledWith(2, "  p")
+    expect(spy).toHaveBeenNthCalledWith(3, "    aaa")
   })
 })
