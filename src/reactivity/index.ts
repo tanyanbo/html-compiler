286377524433b9740ab565e0e1ff7e740295a9f2
Tan Yanbo <tanyanbo95@hotmail.com>
2022-11-11 13:42:34 +0800

    add reactivity code

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -1,3 +1,38 @@
-let depsMap: WeakMap<object, Set<Function>> = new WeakMap()
+let activeEffect: Function | null
 
-function effect(fn: Function) {}
+export class Ref<T> {
+  #value: T
+  subscribers: Function[] = []
+
+  constructor(value: T) {
+    this.#value = value
+  }
+
+  get value() {
+    this.#depend()
+    return this.#value
+  }
+
+  set value(newVal: T) {
+    this.#value = newVal
+    this.#notify()
+  }
+
+  #depend() {
+    if (activeEffect) {
+      this.subscribers.push(activeEffect!)
+    }
+  }
+
+  #notify() {
+    this.subscribers.forEach((sub) => {
+      sub()
+    })
+  }
+}
+
+export function watchEffect(effect: () => void) {
+  activeEffect = effect
+  activeEffect()
+  activeEffect = null
+}
